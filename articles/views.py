14cced43e319fbc99e0b26d3272892254e550bc8
DHL68 <ldh1255@naver.com>
2022-06-07 14:43:39 +0900

    6강 글 작성 페이지

--- articles/views.py
+++ articles/views.py
@@ -1,22 +1,31 @@
 from django.shortcuts import render
 import random
+from .models import Article
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
 
+
 def dinner(request, name):
-    menus = [{"name": '족발', "price": 30000}, {"name": '햄버거', "price": 5000}, {"name": '치킨', "price": 20000}, {"name": '초밥', "price": 15000}]
+    menus = [{"name": '족발', "price": 30000}, {"name": '햄버거', "price": 5000}, {
+        "name": '치킨', "price": 20000}, {"name": '초밥', "price": 15000}]
     pick = random.choice(menus)  # 랜덤으로 딕셔너리 목록을 골라줌
+    articles = Article.objects.all()
     context = {
         'pick': pick,
         'name': name,
         'menus': menus,
+        'articles': articles,
     }
     return render(request, 'dinner.html', context)
 
+
 def review(request):
     return render(request, 'review.html')
+
 
 def create_review(request):
     content = request.POST.get('content')
